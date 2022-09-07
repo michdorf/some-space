42264492c866dd4c77bfe85fd84a01ab4be1137a
Michele Dorph De Chiffre <md@autonik.dk>
2022-09-07 17:43:30 +0200

    Adds Capacitor and iOS build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.michdorf.somespace',
+  appName: 'some-space',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
