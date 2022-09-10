edd5c43cc2b170243df33f897e9e17abe94b5271
Michele Dorph De Chiffre <md@autonik.dk>
2022-09-10 17:41:37 +0200

    Mostra tempo libero in calendario

--- src/lib/Giorno.svelte
+++ src/lib/Giorno.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
+import { tempoLibero } from "../ts/calendario";
+
     import type { Evento as EventoT } from "../types/calendario";
     import Evento from "./Evento.svelte";
+import TempoLibero from "./TempoLibero.svelte";
 
     export let eventi: EventoT[];
     export let data: Date;
+
+    let tempo = tempoLibero(data, eventi);
+    let tEventi: EventoT[] = tempo.map(v => {
+      return {
+        id: crypto.randomUUID(),
+        title: 'Tempo libero',
+        calendar: 'tempo_libero',
+        startDate: v[0],
+        endDate: v[1],
+        lastModified: new Date(0)
+      }
+    });
+
+    let eventiUniti = [...eventi, ...tEventi].sort((a, b) => a.startDate.getTime() - b.startDate.getTime());
 </script>
 
 <div class="data">{data.toLocaleDateString()}</div>
 {#if eventi.length < 1}
   <p style="font-style: italic;">Ingen begivenheder</p>
 {/if}
-{#each eventi as evento}
+{#each eventiUniti as evento}
+  {#if evento.calendar === 'tempo_libero'}
+  <TempoLibero evento={evento}></TempoLibero>
+  {:else}
   <Evento evento={evento}></Evento>
+  {/if}
 {/each}
 
 <style>
---
+++ src/lib/TempoLibero.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type { Evento } from "../types/calendario";
+    export let evento: Evento;
+ 
+    function oriario(evento: Evento): string {
+       return `${evento.startDate.getHours()}:${evento.startDate.getMinutes()} - ${evento.endDate.getHours()}:${evento.endDate.getMinutes()}`;
+    }
+</script>
+
+<div class="evento">
+    <p class="titolo">Tempo libero</p>
+    <p class="orario">{oriario(evento)}</p>
+    <p class="calendario">{evento.calendar}</p>
+  </div>
+
+  <style>
+    .evento p {
+      margin: 0;
+    }
+    .evento {
+      background-color: silver;
+      border: 1px solid blue;
+      color: rgb(0, 0, 152);
+      margin-top: 0.8rem;
+      padding: 8px;
+      text-align: left;
+    }
+    .evento .calendario {
+      text-align: right;
+      font-size: 0.8rem;
+    }
+  </style>
